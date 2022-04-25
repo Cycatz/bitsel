59a9257d617aa08720ab4eaea878aeeb246ab289
Cycatz <thesummernightcat@gmail.com>
2022-04-25 13:29:38 +0800

    Implement AND, OR, NOT and NOT functions

--- include/bitslice.hpp
+++ include/bitslice.hpp
@@ -18,8 +18,9 @@ private:
 public:
     explicit Bits(std::size_t s, std::size_t e = 0);
     explicit Bits(const std::string &str)
-        : m_bitstr{str}, m_start{str.length()}, m_end{0}
+        : m_bitstr{str}, m_start{str.length() - 1}, m_end{0}
     {
+        /* TODO: handle str.length() == 0 */
     }
 
     void reverse();
@@ -38,10 +39,11 @@ public:
     constexpr bool operator[](std::size_t pos) const;
     /* TODO: define set operation */
 
+    bool operator==(const Bits &) const;
     Bits &operator&=(const Bits &);
     Bits &operator|=(const Bits &);
     Bits &operator^=(const Bits &);
-    Bits operator~();
+    Bits operator~() const;
 };
 
 
@@ -83,5 +85,107 @@ Bits Bits::operator()(std::size_t s, std::size_t e) const
     const std::string b = to_string(s, e);
     return Bits(b);
 }
+
+bool Bits::operator==(const Bits &rhs) const
+{
+    return m_bitstr == rhs.m_bitstr && m_start == rhs.m_start &&
+           m_end == rhs.m_end;
+}
+
+
+Bits &Bits::operator&=(const Bits &rhs)
+{
+    std::size_t pos = 0;
+    std::string b;
+
+    while (true) {
+        if (pos >= m_bitstr.size() && pos >= rhs.m_bitstr.size()) {
+            break;
+        }
+        auto c = [&]() {
+            bool a = false;
+            bool b = false;
+            if (pos < m_bitstr.size()) {
+                a = static_cast<bool>(m_bitstr[pos] - '0');
+            }
+            if (pos < rhs.m_bitstr.size()) {
+                b = static_cast<bool>(rhs.m_bitstr[pos] - '0');
+            }
+            return static_cast<char>(static_cast<char>(a && b) + '0');
+        }();
+        b.push_back(c);
+        pos++;
+    }
+
+    m_bitstr = b;
+    return (*this);
+}
+Bits &Bits::operator|=(const Bits &rhs)
+{
+    std::size_t pos = 0;
+    std::string b;
+
+    while (true) {
+        if (pos >= m_bitstr.size() && pos >= rhs.m_bitstr.size()) {
+            break;
+        }
+        auto c = [&]() {
+            bool a = false;
+            bool b = false;
+            if (pos < m_bitstr.size()) {
+                a = static_cast<bool>(m_bitstr[pos] - '0');
+            }
+            if (pos < rhs.m_bitstr.size()) {
+                b = static_cast<bool>(rhs.m_bitstr[pos] - '0');
+            }
+            return static_cast<char>(static_cast<char>(a || b) + '0');
+        }();
+        b.push_back(c);
+        pos++;
+    }
+
+    m_bitstr = b;
+    return (*this);
+}
+Bits &Bits::operator^=(const Bits &rhs)
+{
+    std::size_t pos = 0;
+    std::string b;
+
+    while (true) {
+        if (pos >= m_bitstr.size() && pos >= rhs.m_bitstr.size()) {
+            break;
+        }
+        auto c = [&]() {
+            bool a = false;
+            bool b = false;
+            if (pos < m_bitstr.size()) {
+                a = static_cast<bool>(m_bitstr[pos] - '0');
+            }
+            if (pos < rhs.m_bitstr.size()) {
+                b = static_cast<bool>(rhs.m_bitstr[pos] - '0');
+            }
+            return static_cast<char>(static_cast<char>(a != b) + '0');
+        }();
+        b.push_back(c);
+        pos++;
+    }
+
+    m_bitstr = b;
+    return (*this);
+}
+Bits Bits::operator~() const
+{
+    std::string b;
+    std::for_each(m_bitstr.begin(), m_bitstr.end(), [&](char c) {
+        if ('0' == c) {
+            b.push_back('1');
+        } else {
+            b.push_back('0');
+        }
+    });
+    return Bits{b};
+}
+
 
 #endif  // INCLUDE_BITSLICE_HPP_
--- test/bitslice_test.cc
+++ test/bitslice_test.cc
@@ -37,3 +37,61 @@ TEST(ToStringTestInvalid, BasicAssertions)
     Bits b2{5, 0};
     EXPECT_TRUE(b1.to_string(3, 4) == ""s);
 }
+
+
+TEST(SliceTestValid, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits b1a{3, 0}, b1b{2, 0};
+    EXPECT_TRUE(b1a(2, 0) == b1b);
+    EXPECT_TRUE(b1a(2, 0).to_string() == "000"s);
+
+    Bits b2a{5, 0}, b2b{2, 0};
+    EXPECT_TRUE(b2a(3, 1) == b2b);
+    EXPECT_TRUE(b2a(3, 1).to_string() == "000"s);
+}
+
+
+TEST(ANDTest, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits a{"01010"};
+    Bits b{"01100"};
+
+    a &= b;
+    EXPECT_TRUE(a.to_string() == "01000"s);
+}
+
+TEST(ORTest, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits a{"01010"};
+    Bits b{"01100"};
+
+    a |= b;
+    EXPECT_TRUE(a.to_string() == "01110"s);
+}
+
+TEST(XORTest, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits a{"01010"};
+    Bits b{"01100"};
+
+    a ^= b;
+    EXPECT_TRUE(a.to_string() == "00110"s);
+}
+
+TEST(NOTTest, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits a{"01010"};
+    Bits b{"10101"};
+
+    EXPECT_TRUE(~a == b);
+}
