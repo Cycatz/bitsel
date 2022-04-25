969eaf65f54d2d238dbb3b2cd44a05228d133e0b
Cycatz <thesummernightcat@gmail.com>
2022-04-25 11:51:32 +0800

    Deprecate template and adopt std::string implementation

--- include/bitslice.hpp
+++ include/bitslice.hpp
@@ -1,44 +1,87 @@
 #ifndef INCLUDE_BITSLICE_HPP_
 #define INCLUDE_BITSLICE_HPP_
 
+#include <algorithm>
+#include <bitset>
 #include <cstddef>  // for size_t
-#include <string>   // for string
+#include <stdexcept>
+#include <string>  // for string
 
-constexpr std::size_t get_size(std::size_t S, std::size_t E)
-{
-    return 1 + ((S <= E) ? E - S : S - E);
-}
-
-template <std::size_t S, std::size_t E>
 class Bits
 {
 private:
-    std::string b;
+    std::string m_bitstr;
+    std::size_t m_start, m_end;
+
+    constexpr bool check_range(std::size_t, std::size_t) const;
 
 public:
-    Bits();
-    explicit Bits(const std::string &);
+    explicit Bits(std::size_t s, std::size_t e = 0);
+    explicit Bits(const std::string &str)
+        : m_bitstr{str}, m_start{str.length()}, m_end{0}
+    {
+    }
 
-    Bits reverse();
+    void reverse();
+    constexpr std::size_t get_size() const { return m_start - m_end + 1; }
+    constexpr std::pair<std::size_t, std::size_t> get_width()
+    {
+        return std::make_pair(m_start, m_end);
+    }
 
-    std::string to_string(std::size_t = S, std::size_t = E) const;
 
+    std::string to_string() const { return to_string(m_start, m_end); }
+    std::string to_string(std::size_t s) const { return to_string(s, m_end); }
+    std::string to_string(std::size_t, std::size_t) const;
+
+    Bits operator()(std::size_t, std::size_t) const;
     constexpr bool operator[](std::size_t pos) const;
+    /* TODO: define set operation */
 
-    /* Slice */
-    template <std::size_t T, std::size_t U>
-    Bits operator()(std::size_t = T, std::size_t = U) const;
-
-    template <std::size_t T, std::size_t U>
-    Bits &operator&=(const Bits<T, U> &);
-
-    template <std::size_t T, std::size_t U>
-    Bits &operator|=(const Bits<T, U> &);
-
-    template <std::size_t T, std::size_t U>
-    Bits &operator^=(const Bits<T, U> &);
-
+    Bits &operator&=(const Bits &);
+    Bits &operator|=(const Bits &);
+    Bits &operator^=(const Bits &);
     Bits operator~();
 };
+
+
+Bits::Bits(std::size_t s, std::size_t e) : m_start{s}, m_end{e}
+{
+    if (0 != s && 0 != e) {
+        throw std::invalid_argument("Either start or end must be zero");
+    }
+    std::size_t sz = get_size();
+    m_bitstr.resize(sz, '0');
+}
+
+constexpr bool Bits::check_range(std::size_t s, std::size_t e) const
+{
+    return s >= e && m_start >= s && m_end <= e;
+}
+
+void Bits::reverse()
+{
+    std::reverse(m_bitstr.begin(), m_bitstr.end());
+}
+
+std::string Bits::to_string(std::size_t s, std::size_t e) const
+{
+    if (!check_range(s, e)) {
+        return "";
+    }
+
+    return m_bitstr.substr(0, s - e + 1);
+}
+
+
+Bits Bits::operator()(std::size_t s, std::size_t e) const
+{
+    if (!check_range(s, e)) {
+        throw std::out_of_range("range error");
+    }
+
+    const std::string b = to_string(s, e);
+    return Bits(b);
+}
 
 #endif  // INCLUDE_BITSLICE_HPP_
--- test/bitslice_test.cc
+++ test/bitslice_test.cc
@@ -3,10 +3,37 @@
 
 #include "bitslice.hpp"
 
-TEST(HelloTest, BasicAssertions)
+TEST(StringConstructorTest, BasicAssertions)
 {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+    using namespace std::string_literals;
+
+    Bits b1{"0101"};
+    EXPECT_TRUE(b1.to_string(3, 0) == "0101"s);
+
+
+    Bits b2{"1"};
+    EXPECT_TRUE(b2.to_string(0, 0) == "1"s);
+}
+TEST(ToStringTestValid, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits b1{3, 0};
+    EXPECT_TRUE(b1.to_string() == "0000"s);
+
+    Bits b2{1, 0};
+    EXPECT_TRUE(b2.to_string(1) == "00"s);
+
+    Bits b3{5, 0};
+    EXPECT_TRUE(b3.to_string(3, 2) == "00"s);
+}
+TEST(ToStringTestInvalid, BasicAssertions)
+{
+    using namespace std::string_literals;
+
+    Bits b1{3, 0};
+    EXPECT_TRUE(b1.to_string(5, 4) == ""s);
+
+    Bits b2{5, 0};
+    EXPECT_TRUE(b1.to_string(3, 4) == ""s);
 }
