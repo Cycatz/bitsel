e37682d8e527b3d49a51b5b861cb5b9a20b6aaa8
Cycatz <thesummernightcat@gmail.com>
2022-08-20 13:43:33 +0800

    Refactor tests

--- test/bitslice_test.cc
+++ test/bitslice_test.cc
@@ -14,7 +14,20 @@ using namespace std::string_literals;
 TEST(UIntConstructorTest, BasicTest)
 {
     bits b1{8, 0xAA};
-    EXPECT_TRUE(b1.to_string() == "10101010");
+    EXPECT_EQ(b1, 0xAA_U(8_W));
+
+    bits b2{10, 0xBC};
+    EXPECT_EQ(b2, 0xBC_U(10_W));
+
+    bits b3{64, 0xDEADBEEFDEADBEEF};
+    EXPECT_EQ(b3, 0xDEADBEEFDEADBEEF_U(64_W));
+}
+
+TEST(UIntConstructorTest, AdvancedTest)
+{
+    /* The length is smaller the width of the value */
+    bits b1{4, 0xAA};
+    EXPECT_EQ(b1, 0xA_U(4_W));
 }
 
 
@@ -22,27 +35,34 @@ TEST(BitsBitsStringConstructorTest, BasicTest)
 {
     bitstring bs1{"0xDEADbeef"};
     bits b1{bs1};
-    EXPECT_EQ(b1, 0xDEADBEEF_u(32));
+    EXPECT_EQ(b1, "0xDEADBEEF"_U(32_W));
 
     bitstring bs2{"0o72733"};
-    bits b2{bs2};
-    EXPECT_EQ(b2, 072733_u(15));
+    bits b2{20, bs2};
+    EXPECT_EQ(b2, "0o72733"_U(20_W));
 
     bitstring bs3{"0b1101010101"};
-    bits b3{bs3};
-    EXPECT_EQ(b3, 0b1101010101_u(10));
+    bits b3{30, bs3};
+    EXPECT_EQ(b3, "0b1101010101"_U(30_W));
+}
+
+TEST(BitsBitsStringConstructorTest, AdvancedTest)
+{
+    bitstring bs1{"0xDEADbeef"};
+    bits b1{10, bs1};
+    EXPECT_EQ(b1, "0b1011101111"_U(10_W));
 }
 
 TEST(StringConstructorTest, BasicTest)
 {
-    bits b1{"0101"};
-    EXPECT_TRUE(b1.to_string() == "0101");
+    bits b1{"0b0101"};
+    EXPECT_EQ(b1, "0b0101"_U(4_W));
 }
 
-TEST(StringConstructorTest, LongStringTest)
+TEST(StringConstructorTest, LongWidthTest)
 {
-    bits b1{"010100010101000100001010100010101000100001"};
-    EXPECT_TRUE(b1.to_string() == "010100010101000100001010100010101000100001");
+    bits b1{"0xABCDEF1234567890CAFE"};
+    EXPECT_EQ(b1, "0xABCDEF1234567890CAFE"_U(80_W));
 }
 
 TEST(StringConstructorTest, ZeroLengthTest)
@@ -52,7 +72,20 @@ TEST(StringConstructorTest, ZeroLengthTest)
             try {
                 bits b{""};
             } catch (const std::invalid_argument &e) {
-                EXPECT_STREQ("The length must not be zero", e.what());
+                EXPECT_STREQ("Bit string must prefix with 0x, 0o or 0b!",
+                             e.what());
+                throw;
+            }
+        },
+        std::invalid_argument);
+
+    EXPECT_THROW(
+        {
+            try {
+                bits b{"01010101"};
+            } catch (const std::invalid_argument &e) {
+                EXPECT_STREQ("Bit string must prefix with 0x, 0o or 0b!",
+                             e.what());
                 throw;
             }
         },
@@ -62,42 +95,43 @@ TEST(StringConstructorTest, ZeroLengthTest)
 
 TEST(CopyConstructorTest, BasicTest)
 {
-    bits b1{"010100010101000100001010100010101000100001"};
+    bits b1{"0xDEADBEEF"};
     bits b2(b1);
-    EXPECT_TRUE(b1 == b2);
+    EXPECT_EQ(b1, b2);
 }
 
 
 TEST(MoveConstructorTest, BasicTest)
 {
-    bits b1{"010100010101000100001010100010101000100001"};
+    bits b1{"0xABADBABE"};
     bits b2(std::move(b1));
-    EXPECT_TRUE(b2.to_string() == "010100010101000100001010100010101000100001");
+
+    EXPECT_EQ(b2, "0xABADBABE"_U(32_W));
 }
 
 
 TEST(CopyAssignmentTest, BasicTest)
 {
-    bits b1{"010100010101000100001010100010101000100001"};
-    bits b2{"01010001111l000100001010000010101000100001"};
+    bits b1{"0b010100010101000100001010100010101000100001"};
+    bits b2{"0b010100011110000100001010000110101000100001"};
 
     b2 = b1;
-    EXPECT_TRUE(b1 == b2);
+    EXPECT_EQ(b1, b2);
 }
 
 TEST(MoveAssignmentTest, BasicTest)
 {
-    bits b1{"010100010101000100001010100010101000100001"};
-    bits b2{"01010001111l000100001010000010101000100001"};
+    bits b1{"0b010100010101000100001010100010101000100001"};
+    bits b2{"0b010100011111000100001010000010101000100001"};
 
     b2 = std::move(b1);
-    EXPECT_TRUE(b2.to_string() == "010100010101000100001010100010101000100001");
+    EXPECT_TRUE(b2 == "0b010100010101000100001010100010101000100001"_U());
 }
 
 
 TEST(BitAccessTest, BasicTest)
 {
-    bits b1{"010100010101000100001010100010101000100001"};
+    bits b1{"0b010100010101000100001010100010101000100001"};
     EXPECT_TRUE(b1[0]);
     EXPECT_FALSE(b1[7]);
     EXPECT_TRUE(b1[38]);
@@ -106,38 +140,40 @@ TEST(BitAccessTest, BasicTest)
 
 TEST(InitializerListConstructorTest, BasicTest)
 {
-    bits b1{"0101"};
-    bits b2{"1101"};
+    bits b1{"0b0101"};
+    bits b2{"0b1101"};
 
     bits b3 = {b1, b2};
-    EXPECT_TRUE(b3.to_string() == "01011101"s);
+    EXPECT_EQ(b3, "0b01011101"_U(8_W));
+
+    bits b4{"0xABADBABE"};
+    bits b5{"0xDEADBEEF"};
+
+    bits b6 = {b4, b5};
+    EXPECT_EQ(b6, "0xABADBABEDEADBEEF"_U(64_W));
 }
 
 
 TEST(InitializerListConstructorTest, AdvancedTest)
 {
-    bits b1{"0101101010101010101010101010101010000000001001"};
-    bits b2{"1101101010101010101010101010101010001010101101"};
-    bits b3{"1101111111111111111111111111111100000000000000"};
-    bits b4{"1101111111111111111100000000000000011111111110"};
+    bits b1 = "0xDEADE"_U(20_W);
+    bits b2 = "0xAAAAAA"_U(24_W);
+    bits b3 = "0o124411"_U(18_W);
+    bits b4 = "0b01010101000010"_U(14_W);
 
     bits b = {b1, b2, b3, b4};
-    EXPECT_TRUE(b.to_string() ==
-                "0101101010101010101010101010101010000000001001"
-                "1101101010101010101010101010101010001010101101"
-                "1101111111111111111111111111111100000000000000"
-                "1101111111111111111100000000000000011111111110"s);
+    EXPECT_EQ(b, "0xDEADEAAAAAA2A425542"_U(76_W));
 }
 
 TEST(ToStringTestValid, ZeroArgument)
 {
-    bits b1{"01000101010101000001001010101010010101001010"};
+    bits b1{"0b01000101010101000001001010101010010101001010"};
     EXPECT_TRUE(b1.to_string() ==
                 "01000101010101000001001010101010010101001010"s);
 }
 TEST(ToStringTestValid, OneArgument)
 {
-    bits b1{"01000101010101000001001010101010010101001010"};
+    bits b1{"0b01000101010101000001001010101010010101001010"};
     EXPECT_TRUE(b1.to_string(1) == "10");
     EXPECT_TRUE(b1.to_string(4) == "01010");
     EXPECT_TRUE(b1.to_string(40) ==
@@ -164,97 +200,94 @@ TEST(ToStringTestValid, TwoArgument)
 
 TEST(SliceTestValid, BasicTest)
 {
-    bits b1a{4}, b2a{"010101000"};
-    EXPECT_TRUE(b1a(2, 0).to_string() == "000"s);
-    EXPECT_TRUE(b2a(3).to_string() == "1000"s);
+    bits b1a = bits::zeros(4), b2a{"0b010101000"};
+
+    EXPECT_EQ(b1a(2, 0), "0b000"_U());
+    EXPECT_EQ(b2a(3, 1), "0b100"_U());
 }
 
 TEST(SliceTestValid, AdvancedTest)
 {
-    bits b2a{"101010110101010010001001010101000000010101010000001000"};
-    bits b2b{"10101001000100101010100000001010"};
-    EXPECT_TRUE(b2a(44, 13) == b2b);
+    bits b2a{"0b101010110101010010001001010101000000010101010000001000"};
+    bits b2b{"0b10101001000100101010100000001010"};
+    EXPECT_EQ(b2a(44, 13), b2b);
 }
 
 TEST(GetNBitsTest, BasicTest)
 {
-    bits b = 0xDEADBEEF_u(32);
+    bits b = 0xDEADBEEF_U(32_W);
     auto i = b.get_nbits(0, 6);
     EXPECT_TRUE(i == 0x2f);
 
-    bits c = 0xDEADBEEFDEADBEEF_u(64);
+    bits c = 0xDEADBEEFDEADBEEF_U(64_W);
     i = c.get_nbits(20, 32);
     EXPECT_TRUE(i == 0xDBEEFDEA);
 }
 
 TEST(SetNBitsTest, BasicTest)
 {
-    bits b = 0xDEADBEEF_u(32);
+    bits b = 0xDEADBEEF_U(32_W);
     b.set_nbits(0x1E, 0, 6);
-    EXPECT_TRUE(b == 0xDEADBEDE_u(32));
+    EXPECT_TRUE(b == 0xDEADBEDE_U(32_W));
 
-    bits c = 0xDEADBEEFDEADBEEF_u(64);
+    bits c = 0xDEADBEEFDEADBEEF_U(64_W);
     c.set_nbits(0xFEEDBABE, 20, 32);
-    EXPECT_TRUE(c == 0xDEAFEEDBABEDBEEF_u(64));
+    EXPECT_TRUE(c == 0xDEAFEEDBABEDBEEF_U(64_W));
 }
 
 
 
 TEST(RepeatTest, BasicTest)
 {
-    bits a{"01010"};
+    bits a{"0b01010"};
 
     a.repeat(3);
-
-    EXPECT_TRUE(a.to_string() == "010100101001010"s);
+    EXPECT_EQ(a, "0b010100101001010"_U());
 }
 
 
 TEST(AppendTest, BasicTest)
 {
-    bits a{"01010"};
-    bits b{"01100"};
+    bits a{"0b01010"};
+    bits b{"0b01100"};
 
     a.append(b);
-    EXPECT_TRUE(a.to_string() == "0101001100"s);
+    EXPECT_EQ(a, "0b0101001100"_U());
 }
 
 TEST(AppendTest, AdvancedTest)
 {
-    bits a{
-        "0101011111111111111111111111111111111111111111111110000000000000100000"
-        "00"};
-    bits b{"01100"};
+    bits a{"0xBADBABE"};
+    bits b{"0xEBABDAB"};
 
     a.append(b);
-    EXPECT_TRUE(
-        a.to_string() ==
-        "01010111111111111111111111111111111111111111111111100000000000001000000001100"s);
+    EXPECT_EQ(a, "0xBADBABEEBABDAB"_U());
 }
 
-TEST(ReplicationTest, BasicTest)
+TEST(FillTest, BasicTest)
 {
-    bits a{"011"};
+    bits a{"0b011"};
+    bits b = Fill(2, Fill(3, a));
+    EXPECT_EQ(b, "0b011011011011011011"_U());
 
-    bits b = repl(2, repl(3, a));
-    EXPECT_TRUE(b.to_string() == "011011011011011011"s);
+    bits c{"0xDEADC"};
+    bits d = Fill(3, Fill(1, c));
+    EXPECT_EQ(d, "0xDEADCDEADCDEADC"_U());
 }
 
-TEST(ConcatTest, BasicTest)
+TEST(CatTest, BasicTest)
 {
-    bits a{"01010"};
-    bits b{"01100"};
-    bits c{"11001"};
+    bits a{"0b01010"};
+    bits b{"0b01100"};
+    bits c{"0b11001"};
 
-    bits d = concat(a, concat(b, c));
-    EXPECT_TRUE(d.to_string() == "010100110011001"s);
+    bits d = Cat(a, Cat(b, c));
+    EXPECT_EQ(d, "0b010100110011001"_U());
 
     bits e{28, 0xEADBEEF};
     bits f{40, 0xDDDEADBEEF};
-    bits g = {f, e};
-    EXPECT_TRUE(g(67, 64) == 0xD_u(4));
-    EXPECT_TRUE(g(63, 32) == 0xDDEADBEE_u(32));
-    EXPECT_TRUE(g(31, 0) == 0xFEADBEEF_u(32));
+    bits g = Cat(e, f);
+    EXPECT_EQ(g, "0xEADBEEFDDDEADBEEF"_U());
 }
 
 
@@ -262,116 +295,95 @@ TEST(RightShiftTest, BasicTest)
 {
     bits a{64, 0xDEADBEEF12345678};
     a >>= 4;
-    EXPECT_TRUE(a == 0x0DEADBEEF1234567_u(64));
+    EXPECT_TRUE(a == 0x0DEADBEEF1234567_U(64_W));
     a >>= 1;
-    EXPECT_TRUE(a == 0x06F56DF77891A2B3_u(64));
+    EXPECT_TRUE(a == 0x06F56DF77891A2B3_U(64_W));
     a >>= 7;
-    EXPECT_TRUE(a == 0x000DEADBEEF12345_u(64));
+    EXPECT_TRUE(a == 0x000DEADBEEF12345_U(64_W));
     a >>= 20;
-    EXPECT_TRUE(a == 0x00000000DEADBEEF_u(64));
+    EXPECT_TRUE(a == 0x00000000DEADBEEF_U(64_W));
     a >>= 100000;
-    EXPECT_TRUE(a == 0_u(64));
+    EXPECT_TRUE(a == 0_U(64_W));
 }
 
 TEST(LeftShiftTest, TwoBlockTest)
 {
     bits a{36, 0x1DEADBEEF};
     a <<= 4;
-    EXPECT_TRUE(a == 0xDEADBEEF0_u(36));
+    EXPECT_TRUE(a == 0xDEADBEEF0_U(36_W));
     a <<= 3;
-    EXPECT_TRUE(a == 0xF56DF7780_u(36));
+    EXPECT_TRUE(a == 0xF56DF7780_U(36_W));
     a <<= 9;
-    EXPECT_TRUE(a == 0xDBEEF0000_u(36));
+    EXPECT_TRUE(a == 0xDBEEF0000_U(36_W));
 }
 
 TEST(LeftShiftTest, SingleBlockTest)
 {
     bits a{16, 0xDEAD};
     a <<= 8;
-    EXPECT_TRUE(a == 0xAD00_u(16));
+    EXPECT_TRUE(a == 0xAD00_U(16_W));
 }
 
 TEST(LeftShiftTest, SingleFullBlockTest)
 {
     bits a{32, 0x12345678};
     a <<= 8;
-    EXPECT_TRUE(a == 0x34567800_u(32));
+    EXPECT_TRUE(a == 0x34567800_U(32_W));
 }
 
 TEST(LeftShiftTest, LargeShiftTest)
 {
     bits a{8, 0b10010101};
     a <<= 100000;
-    EXPECT_TRUE(a == 0b00000000_u(8));
-}
-
-TEST(AdditionTest, BasicTest)
-{
-    bits a{"010"};
-    bits b{"110"};
-    bits c = a + b;
-
-    EXPECT_TRUE(c.to_string() == "010110"s);
-}
-
-TEST(MultiplicationTest, BasicTest)
-{
-    bits a{"101"};
-    bits b = 3 * a;
-    bits c = 2 * b;
-
-    EXPECT_TRUE(b.to_string() == "101101101"s);
-    EXPECT_TRUE(c.to_string() == "101101101101101101"s);
+    EXPECT_TRUE(a == 0b00000000_U(8_W));
 }
 
 
 TEST(ANDTest, BasicTest)
 {
-    bits a{"01010"};
-    bits b{"01100"};
-    bits c{"00101"};
+    bits a{"0xDEADBEEF224"};
+    bits b{"0xABADBABE828"};
+    bits c{"0o1222"};
 
     a &= b;
-
-    EXPECT_TRUE(a.to_string() == "01000"s);
-
+    EXPECT_EQ(a, "0x8AADBAAE020"_U(44_W));
 
     bits d = a & c;
-    EXPECT_TRUE(d.to_string() == "00000"s);
+    EXPECT_EQ(d, "0x0"_U(44_W));
 }
 
 TEST(ORTest, BasicTest)
 {
-    bits a{"01010"};
-    bits b{"01100"};
-    bits c{"00101"};
+    bits a{"0xDEADBEEF224"};
+    bits b{"0xABADBABE828"};
+    bits c{"0o1222"};
 
     a |= b;
-    EXPECT_TRUE(a.to_string() == "01110"s);
+    EXPECT_EQ(a, "0xFFADBEFFA2C"_U(44_W));
 
     bits d = a | c;
-    EXPECT_TRUE(d.to_string() == "01111"s);
+    EXPECT_EQ(d, "0xFFADBEFFABE"_U(44_W));
 }
 
 TEST(XORTest, BasicTest)
 {
-    bits a{"01010"};
-    bits b{"01100"};
-    bits c{"00101"};
+    bits a{"0xDEADBEEF224"};
+    bits b{"0xABADBABE828"};
+    bits c{"0o1222"};
 
     a ^= b;
-    EXPECT_TRUE(a.to_string() == "00110"s);
+    EXPECT_EQ(a, "0x75000451A0C"_U(44_W));
 
     bits d = a ^ c;
-    EXPECT_TRUE(d.to_string() == "00011"s);
+    EXPECT_EQ(d, "0x7500045189E"_U(44_W));
 }
 
 TEST(NOTTest, BasicTest)
 {
-    bits a{"01010"};
-    bits b{"10101"};
+    bits a{"0xDEADBEEF1234567890"};
+    bits b{"0x21524110edcba9876f"};
 
-    EXPECT_TRUE(~a == b);
+    EXPECT_EQ(~a, b);
 }
 
 
